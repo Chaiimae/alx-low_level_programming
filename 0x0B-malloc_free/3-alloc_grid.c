42a3e1a4c1d88aa3c7ef5e8cfdb0180a583ab5ab
Chaiimae <chaimae.lahdili@gmail.com>
2023-05-16 13:43:55 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -5,7 +5,7 @@
  * @width: width input
  * @height: height input
  *
- * return: pointer to 2 dimensional array
+ * Return: pointer to 2 dimensional array
  */
 int **alloc_grid(int width, int height)
 {
